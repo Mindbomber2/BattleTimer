0ad980b2250f149498dee249ea65aa324cc4087b
Bailey <erringtonbusiness@gmail.com>
2021-04-28 13:47:24 +0100

    Wow! not doing this crashes the game!

--- src/main/java/BattleTimer/mechanics/constants/personalities/VERYEASY.java
+++ src/main/java/BattleTimer/mechanics/constants/personalities/VERYEASY.java
@@ -5,7 +5,10 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 public class VERYEASY extends AbstractPersonality {
 
     @Override
-    public float calculateTimeValue() { return AbstractDungeon.monsterRng.random(0, -1); }
+    public float calculateTimeValue() {
+        try { return AbstractDungeon.monsterRng.random(0, -1); }
+        catch (Exception e){ return 0; }
+    }
 
     @Override
     public AbstractPersonality nextPersonality() { return new EASY(); }
